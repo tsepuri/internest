4e33a1a468d465f0872f341d87e20cb8bf1fa391
SeongIkKim <kpic1638@gmail.com>
2023-02-26 07:21:19 -0500

    feat: update interface for client

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
 
-from dependencies import get_token_header
-from routers import clerk, user
 from es_client import get_es_client
 
 app = FastAPI()
@@ -19,19 +17,37 @@ async def get_user_graph_map():
     get all nested document related to user
     :return:
     """
-    pass
-    return {"graph-map": "something"}
+
+    return {"graph": ["people", {"place" : ["OSU", "Cincinaty"]},  "food", ""]}
 
 
-@app.post("/journal")
-async def create_user_journal():
+@app.post("/parse-journal")
+async def parse_user_journal():
     """
-    create new journal in graphmap
+    parse user input journal and create journal document.
+    extract main keywords in journal and return them to client for user validation.
     :return:
     """
     pass
-    return {"something": "something"}
+    # user auth
+    # db.insert_journal_entry
+    # openai model extracts keyword from journal
+    return {"extractedKeywords": ["keyword1", "keywordA", "keyword3", "keyword4", "keyword5"]}
 
+
+@app.post("/validated-keywords")
+async def register_validated_keywords():
+    pass
+    # user auth
+    # create keyword docs in es
+    # get similar existing keywords recommendation for decide parent node(categorize)
+    # update keywords and update graphmap
+    graph = ["keyword1", {"keyword2": ["keyword3", "keyword4"]}, "keyword5"]
+    frequency = {"keyword1": 3, "keyword2": 1, "keyword3": 5, "keyword4": 1, "keyword5": 1}
+    return {
+        "graph" : graph,
+        "frequency" : frequency
+    }
 
 
 @app.post("/keyword")
