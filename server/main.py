e12b6cf8e013456b6b9912945035de1aedd2d512
SeongIkKim <kpic1638@gmail.com>
2023-02-26 00:10:39 -0500

    feat: add pydantic models

--- server/main.py
+++ server/main.py
@@ -6,3 +6,23 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/{user_id}/graph-map")
+async def get_user_graph_map():
+    """
+    get all nested document related to user
+    :return:
+    """
+    pass
+    return {"graph-map": "something"}
+
+
+@app.post("/journal")
+async def create_user_journal():
+    """
+    create new journal in graphmap
+    :return:
+    """
+    pass
+    return {"something": "something"}
---
+++ server/models.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+from typing import ForwardRef, List
+
+from bson import ObjectId
+from pydantic import BaseModel, Field, EmailStr
+
+from db import PyObjectId
+
+
+class TimestampedModel(BaseModel):
+    createdAt: datetime
+    updatedAt: datetime
+
+
+class User(TimestampedModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str
+    email: EmailStr
+    graph: list = Field(default_factory=list)
+
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {
+            datetime: lambda dt: dt.isoformat(),
+            ObjectId: str
+        }
+        schema_extra = {
+            "exampleUser": {
+                "name": "Simba",
+                "email": "simba@example.com",
+                "graphMap": ["keyword1", {"keyword2": ["keyword3", "keyword4"]}, "keyword5"]
+            }
+        }
+
+
+Keyword = ForwardRef('Keyword')
+
+class Keyword(TimestampedModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    user: User
+    name: str
+    parent: Keyword = None
+    children: List[Keyword] = Field(default_factory=list)
+
+    schema_extra = {
+        "exampleKeyword": {
+            "name": "restaurant",
+            "parent": "place",
+            "children": ["Chipolet", "BIBIBOP"]
+        }
+    }
+
+
+class Journal(TimestampedModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    user: User
+    content: str
+
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {
+            datetime: lambda dt: dt.isoformat(),
+            ObjectId: str
+        }
+        schema_extra = {
+            "exampleJournal": {
+                datetime: lambda dt: dt.isoformat(),
+                ObjectId: str
+            }
+        }
