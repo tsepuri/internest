71b91e06ec3da9a5fd7f5b27e6a4ee52e9127fd8
Hunter Welch <hjw36@case.edu>
2023-02-26 12:22:21 -0500

    graph stuff

--- client/src/pages/graph.tsx
+++ client/src/pages/graph.tsx
@@ -12,6 +12,7 @@ import { useAuth } from '@clerk/nextjs';
 type Node = {
   id: number;
   label: string;
+  size: number;
 };
 
 type Edge = {
@@ -29,28 +30,60 @@ const Graph = () => {
             setGraphResponse(data)
           }
       }, [])
-/*
-  let graphResponse:GraphResponse = {
-    graph: [{"keyword1": {"keyword2": ["keyword3", "keyword4"]}, "keyword5": {}}],
-    frequency: {"keyword1": 3, "keyword2": 1, "keyword3": 5, "keyword4": 1, "keyword5": 1}
-  }
-  */
-  const nodes: Node[] = [
-    { id: 1, label: 'User' },
-    { id: 2, label: 'People' },
-    { id: 3, label: 'Ike' },
-    { id: 4, label: 'Tarun' },
-    { id: 5, label: 'Food' },
-    { id: 6, label: 'Pizza' },
-  ];
 
-  const edges: Edge[] = [
-    { from: 1, to: 2 },
-    { from: 1, to: 5 },
-    { from: 2, to: 3 },
-    { from: 2, to: 4 },
-    { from: 5, to: 6 },
-  ];
+  // let graphResponse:GraphResponse = {
+  //   graph: [{"keyword1": {"keyword2": ["keyword3", "keyword4"]}, "keyword5": {}}],
+  //   frequency: {"keyword1": 3, "keyword2": 1, "keyword3": 5, "keyword4": 1, "keyword5": 1}
+  // }
+
+  var uniqueIDCounter = 0;
+  var keysArr = Object.keys((graphResponse as GraphResponse).frequency);
+  var averageFreq = Object.values((graphResponse as GraphResponse).frequency).reduce((a, b) => a + b) / keysArr.length;
+  var i = 0;
+
+  const nodes = keysArr.map((key, i) => {
+    var currentFreq = (graphResponse as GraphResponse).frequency[key] 
+    var currentSize = 0;
+    var size = 15;
+
+    if (currentFreq > averageFreq) {
+      size = 22;
+    }
+
+    return { id: i, label: key, size: size }
+
+  });
+
+  const userNode = {id: -1, label: "User", size: 20};
+  nodes.push(userNode);
+
+  const forest0 = (graphResponse as GraphResponse).graph[0]
+
+  function dfs(forest: any, parent: Node, f: { (t: any, p: any): void; (arg0: string, arg1: any): void; }) {
+    if (Array.isArray(forest)) {
+      Object.values(forest).forEach((treeLabel: string) => {
+        var labelId = keysArr.indexOf(treeLabel)
+        const treeNode = { id: keysArr.indexOf(treeLabel), label: treeLabel, size: 0 };
+
+        f(treeNode, parent);
+      })
+    } else if (typeof forest == "object") {
+      Object.keys(forest).forEach((treeLabel: string) => {
+        const treeNode = { id: keysArr.indexOf(treeLabel), label: treeLabel, size: 0 };
+
+        f(treeNode, parent);
+        const children = forest[treeLabel];
+
+        dfs(children, treeNode, f);
+      })
+    }
+  }
+
+  const edges:Edge[] = [];
+
+  dfs(forest0, userNode, (t: any, p: any) => {
+    edges.push({from: p.id, to: t.id});
+  });
 
   const [selectedNodeId, setSelectedNodeId] = useState<number | null>(null);
   
--- client/src/services/GraphService.ts
+++ client/src/services/GraphService.ts
@@ -2,7 +2,7 @@ import { User } from "@/types/User";
 import { api } from "./axios";
 import { makeSessionInfo } from "./clerk";
 
-async function getGraph(user:User, sessionId:string) {
+export async function getGraph(user:User, sessionId:string) {
     let response = await api.get(`/${user.userId}/graph-map`)
     return response.data
 }
